38455be9ca318d0143dd7f508a8e2b994de37cc6
FedericoBarberon <jfedericobarberonj@gmail.com>
2026-01-16 01:42:15 -0300

    encapsulate Team and Match state in props object

--- src/domain/match/Match.ts
+++ src/domain/match/Match.ts
@@ -6,38 +6,94 @@ import { InvalidGoalsError, InvalidPlayerNameError, SamePlayerError, SameTeamsEr
 
 export type MatchId = string
 
-export class Match {
-    private constructor(
-        public readonly id: MatchId,
-        public homePlayer: string,
-        public awayPlayer: string,
-        public homeTeamId: TeamId,
-        public awayTeamId: TeamId,
-        public homeGoals: number,
-        public awayGoals: number,
-        public date: Date
-    ) { }
+export type MatchProps = {
+    id: MatchId
+    date: Date
+    homeTeamId: TeamId
+    homePlayer: string
+    homeGoals: number
+    awayTeamId: TeamId
+    awayPlayer: string
+    awayGoals: number
+}
 
-    static create(props: {
-        id: MatchId
-        homePlayer: string
-        awayPlayer: string
-        homeTeamId: TeamId
-        awayTeamId: TeamId
-        homeGoals: number
-        awayGoals: number
-        date: Date
-    }): Result<Match, DomainError> {
-        const match = new Match(
-            props.id,
-            formatName(props.homePlayer),
-            formatName(props.awayPlayer),
-            props.homeTeamId,
-            props.awayTeamId,
-            props.homeGoals,
-            props.awayGoals,
-            props.date
-        )
+export type CreateMatchProps = MatchProps
+export type UpdateMatchProps = Partial<Omit<MatchProps, "id">>
+
+export class Match {
+    private constructor(private props: MatchProps) { }
+
+    get id(): MatchId {
+        return this.props.id
+    }
+
+    get homePlayer(): string {
+        return this.props.homePlayer
+    }
+
+    get awayPlayer(): string {
+        return this.props.awayPlayer
+    }
+
+    get homeTeamId(): TeamId {
+        return this.props.homeTeamId
+    }
+
+    get awayTeamId(): TeamId {
+        return this.props.awayTeamId
+    }
+
+    get homeGoals(): number {
+        return this.props.homeGoals
+    }
+
+    get awayGoals(): number {
+        return this.props.awayGoals
+    }
+
+    get date(): Date {
+        return this.props.date
+    }
+
+    private set homePlayer(value: string) {
+        this.props.homePlayer = value
+    }
+
+    private set awayPlayer(value: string) {
+        this.props.awayPlayer = value
+    }
+
+    private set homeTeamId(value: TeamId) {
+        this.props.homeTeamId = value
+    }
+
+    private set awayTeamId(value: TeamId) {
+        this.props.awayTeamId = value
+    }
+
+    private set homeGoals(value: number) {
+        this.props.homeGoals = value
+    }
+
+    private set awayGoals(value: number) {
+        this.props.awayGoals = value
+    }
+
+    private set date(value: Date) {
+        this.props.date = value
+    }
+
+    static create(props: CreateMatchProps): Result<Match, DomainError> {
+        const match = new Match({
+            id: props.id,
+            date: props.date,
+            homeTeamId: props.homeTeamId,
+            homePlayer: formatName(props.homePlayer),
+            homeGoals: props.homeGoals,
+            awayTeamId: props.awayTeamId,
+            awayPlayer: formatName(props.awayPlayer),
+            awayGoals: props.awayGoals
+        })
 
         const validation = match.validate()
         if (!validation.ok) return validation
@@ -45,22 +101,14 @@ export class Match {
         return Ok(match)
     }
 
-    edit(props: {
-        homePlayer?: string
-        awayPlayer?: string
-        homeTeamId?: TeamId
-        awayTeamId?: TeamId
-        homeGoals?: number
-        awayGoals?: number
-        date?: Date
-    }): Result<void, DomainError> {
-        this.homePlayer = props.homePlayer ? formatName(props.homePlayer) : this.homePlayer
-        this.awayPlayer = props.awayPlayer ? formatName(props.awayPlayer) : this.awayPlayer
-        this.homeTeamId = props.homeTeamId ?? this.homeTeamId
-        this.awayTeamId = props.awayTeamId ?? this.awayTeamId
-        this.homeGoals = props.homeGoals ?? this.homeGoals
-        this.awayGoals = props.awayGoals ?? this.awayGoals
-        this.date = props.date ?? this.date
+    edit(props: UpdateMatchProps): Result<void, DomainError> {
+        if (props.homePlayer !== undefined) this.homePlayer = formatName(props.homePlayer)
+        if (props.awayPlayer !== undefined) this.awayPlayer = formatName(props.awayPlayer)
+        if (props.homeTeamId !== undefined) this.homeTeamId = props.homeTeamId
+        if (props.awayTeamId !== undefined) this.awayTeamId = props.awayTeamId
+        if (props.homeGoals !== undefined) this.homeGoals = props.homeGoals
+        if (props.awayGoals !== undefined) this.awayGoals = props.awayGoals
+        if (props.date !== undefined) this.date = props.date
 
         const validation = this.validate()
         if (!validation.ok) return validation
--- src/domain/team/Team.ts
+++ src/domain/team/Team.ts
@@ -5,18 +5,44 @@ import { InvalidTeamNameError } from "./errors"
 
 export type TeamId = string
 
-export class Team {
-    private constructor(
-        public readonly id: TeamId,
-        public name: string,
-        public isArchived: boolean
-    ) { }
+export type TeamProps = {
+    id: TeamId
+    name: string
+    isArchived: boolean
+}
 
-    static create(props: {
-        id: TeamId,
-        name: string,
-    }): Result<Team, DomainError> {
-        const team = new Team(props.id, formatName(props.name), false)
+export type CreateTeamProps = Omit<TeamProps, "isArchived">
+export type UpdateTeamProps = Partial<Omit<TeamProps, "id">>
+
+export class Team {
+    private constructor(private props: TeamProps) { }
+
+    get id(): TeamId {
+        return this.props.id
+    }
+
+    get name(): string {
+        return this.props.name
+    }
+
+    get isArchived(): boolean {
+        return this.props.isArchived
+    }
+
+    private set name(value: string) {
+        this.props.name = value
+    }
+
+    private set isArchived(value: boolean) {
+        this.props.isArchived = value
+    }
+
+    static create(props: CreateTeamProps): Result<Team, DomainError> {
+        const team = new Team({
+            id: props.id,
+            name: formatName(props.name),
+            isArchived: false
+        })
         const validation = team.validate()
 
         if (!validation.ok) return validation
@@ -24,12 +50,9 @@ export class Team {
         return Ok(team)
     }
 
-    edit(props: {
-        name?: string
-        isArchived?: boolean
-    }): Result<void, DomainError> {
-        this.name = props.name ? formatName(props.name) : this.name
-        this.isArchived = props.isArchived ?? this.isArchived
+    edit(props: UpdateTeamProps): Result<void, DomainError> {
+        if (props.name !== undefined) this.name = formatName(props.name)
+        if (props.isArchived !== undefined) this.isArchived = props.isArchived
 
         const validation = this.validate()
         if (!validation.ok) return validation
